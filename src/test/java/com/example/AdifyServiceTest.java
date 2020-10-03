3a80a27817ea737ffa9fb1c027b9470dd3165e6b
Alexander Nissen <alexander@feldbech.dk>
2020-10-03 16:05:51 +0200

    fetch-product-page end-to-end test

--- src/test/java/com/example/AdifyServiceTest.java
+++ src/test/java/com/example/AdifyServiceTest.java
@@ -9,20 +9,23 @@ class AdifyServiceTest {
 
     class SenderSpy implements Subscription.Sender {
         String event;
+        String body;
 
         @Override
         public void send(String event, String body) {
             this.event = event;
+            this.body = body;
         }
     }
 
     @Test
     @Tag("slow")
-    void foo() {
+    void onFetchProductPageSendDisplay() {
         SenderSpy spy = new SenderSpy();
         AdifyService a = new AdifyService(new Adify(new HerokuGetRequest("adify")), "SESSION_ID,USER_ID,PRODUCT_ID", spy);
         a.execute();
         assertEquals("display", spy.event);
+        assertEquals("SESSION_ID,USER_ID,PRODUCT_ID,TODO", spy.body);
     }
 
 }
